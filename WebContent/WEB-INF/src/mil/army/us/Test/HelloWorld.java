07d8c53abfaf951325f73db3b3b2b9fd77a86c9f
Gregory Washington <glunlimited@gmail.com>
2017-10-11 14:44:16 -0400

    Initial commit of project contents

---
+++ WebContent/WEB-INF/src/mil/army/us/Test/HelloWorld.java
@@ -0,0 +1,13 @@
+package mil.army.us.Test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!"); // Display the string.
+	}
+
+}
---
+++ WebContent/WEB-INF/src/mil/army/us/Test/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package mil.army.us.Test;
+
+public class HelloWorldApp {
+
+	public HelloWorldApp() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void writeMessage(){
+		System.out.println("Hello World App!"); // Display the string.
+	}
+
+}
